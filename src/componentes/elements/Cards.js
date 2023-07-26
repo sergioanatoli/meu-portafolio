622649c46f158c2be5b31893682213d84cbc97af
Sergio Anatoli <sergioanatoli@gmail.com>
2023-07-26 15:20:09 -0500

    Version 2

--- src/componentes/elements/Cards.js
+++ src/componentes/elements/Cards.js
@@ -1,19 +1,33 @@
 import styles from './Cards.module.css'
 import ButtonB from './ButtonB'
-
+import { useState } from 'react'
 
 function Cards({img, title, tech, description, repo, site}){
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOf(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.cards}>
-            <a href={site}>
+        <div onMouseLeave={InfoOf} className={styles.cards}>
+            <a onMouseEnter={InfoOn} href={site}>
             <img src={img} alt='ERROR'/>
             </a>
-            <section>
+
+            {info === true &&(
+                <section>
                 <h3>{title}</h3>
                 <p><strong>Tecnologia:</strong>{tech}</p>
                 <p>{description}</p>
                 <ButtonB text='Go to repository' link={repo}/>
             </section>
+            )}
         </div>
     )
 }
--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,12 +1,44 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
-import ButtonB from '../elements/ButtonB'
+import { useState, useEffect } from 'react'
 
 function Presentation() {
+    const [text, setText] = useState('');
+    const toRotate = ['Sergio Anatoli!', 'Front End Developer', 'Full Stack Developer'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+        
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+    }
+
     return(
         <div className={styles.presentation} id="Presentation">
             <h4><strong>Welcome to my portfolio</strong></h4>
-            <h1>Hi, I'm Sergio Anatoli</h1>
+            <h1>Hi, I'm {text}</h1>
             <p>I'm a passionate and versatile full stack developer. <br/>
                 With a deep-rooted passion for all things tech and a knack for problem-solving, <br/>
                 I thrive on transforming ideas into reality through elegant and efficient code. <br/>
